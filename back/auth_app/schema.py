0cb87eac4436f65d31dad06b1171579a4a19a3c6
Hoseong Son <lsookcha@me.com>
2023-02-03 20:45:55 +0900

    Add email register

--- back/auth_app/schema.py
+++ back/auth_app/schema.py
@@ -1,6 +1,29 @@
 import graphene
 
-class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
+from graphene_django import DjangoObjectType
 
-schema = graphene.Schema(query=Query)
+from .models import User, EmailUser
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+
+
+class EmailRegisterMutation(graphene.Mutation):
+    class Arguments:
+        # The input arguments for this mutation
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    # The class attributes define the response of the mutation
+    emailRegister = graphene.Field(UserType)
+
+    @classmethod
+    def mutate(cls, root, info, text, id):
+        # Create a new user and return it
+        return EmailRegisterMutation()
+
+
+class Mutation(graphene.ObjectType):
+    email_register = EmailRegisterMutation.Field()
