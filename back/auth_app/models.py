c1e2dcf1188b82157de501d9d0eca221bad21beb
Hoseong Son <lsookcha@me.com>
2023-02-03 20:33:13 +0900

    Email/PW 추가

--- back/auth_app/models.py
+++ back/auth_app/models.py
@@ -1,14 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
-    first_name = last_name = None
+class User(models.Model):
+    EMAIL = 'EM'
+    SIGN_UP_METHOD_CHOICES = [
+        (EMAIL, 'EMAIL'),
+    ]
 
-    def __str__(self) -> str:
-        return f"[{self.pk}] {self.username}"
+    signup_method = models.CharField(max_length=2, choices=SIGN_UP_METHOD_CHOICES,default=EMAIL)
+    username = models.TextField()
+    picture_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='생성 시각')
+    updated_at = models.DateTimeField(auto_now_add=True, verbose_name='갱신 시각')
 
     class Meta:
         db_table = 'users'
-        verbose_name = '사용자'
-        verbose_name_plural = '사용자 목록'
+
+class EmailUser(models.Model):
+    email = models.EmailField(verbose_name='유저의 이메일')
+    password = models.TextField(verbose_name='유저의 비밀번호')
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='생성 시각')
+    updated_at = models.DateTimeField(auto_now_add=True, verbose_name='갱신 시각')
--- back/config/settings.py
+++ back/config/settings.py
@@ -77,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -91,26 +90,6 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
@@ -133,5 +112,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# 기본 사용자 설정
-AUTH_USER_MODEL = 'auth_app.User'
