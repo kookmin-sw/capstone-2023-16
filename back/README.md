7a70c95007531f15864a35c20b34fc43e2ada0eb
Hoseong Son <lsookcha@me.com>
2023-02-03 20:07:14 +0900

    Mysql pip 추가

--- back/README.md
+++ back/README.md
@@ -7,9 +7,16 @@
 
 ### 서버 띄우기 Script
 ```
+brew install mysql
+brew install mysql-connector-c
+
 docker-compose up -d ;; mysql db 띄우기
 
 python -m venv venv
 pip install -r requirements.txt
 python manage.py runserver
 ```
+
+
+### GraphQL 실행창 띄우기
+http://127.0.0.1:8000/graphql
--- back/config/settings.py
+++ back/config/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'capstone',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
