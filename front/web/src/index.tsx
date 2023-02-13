641026ce729acde50bb1ead307b3ebe5e311a2b1
Chaemin-L <chaem12@kookmin.ac.kr>
2023-02-14 01:36:33 +0900

    Remove unused files

--- front/web/src/index.tsx
+++ front/web/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,7 +8,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
 
