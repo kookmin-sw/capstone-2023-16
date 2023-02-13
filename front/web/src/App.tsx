6c96011d0468756f4a5d172ed10b2c6e6ef0e85f
Chaemin-L <chaem12@kookmin.ac.kr>
2023-02-14 02:39:51 +0900

    feat: Set up Router

--- front/web/src/App.tsx
+++ front/web/src/App.tsx
@@ -1,12 +1,14 @@
-import { GlobalStyles } from "./styles/GlobalStyles";
+import { Routes, Route } from 'react-router-dom';
+import { GlobalStyles } from './styles/GlobalStyles';
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <div>
-        테스트
-      </div>
+      <Routes>
+        <Route path='/' element={<div>로그인페이지(홈)</div>} />
+        <Route path='/signUp' element={<div>회원가입페이지</div>} />
+      </Routes>
     </>
   );
 }
