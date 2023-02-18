8a18aa4f1f43a5040c2cb76ba5d288ad6eef06c3
Hoseong Son <lsookcha@me.com>
2023-02-18 17:03:44 +0900

    인증 여부 체크하는 decorator 함수 작성 (#6)
    * 유저 스키마 분리
    
    * user 정보 return
    
    * auth required decorator 추가

---
+++ back/auth_app/schemas/user.py
@@ -0,0 +1,41 @@
+import strawberry
+
+from auth_app.models import User
+from django.contrib.auth import authenticate, login
+from strawberry.types.info import Info
+from .types import User as UserType
+
+
+@strawberry.type
+class LoginSuccess:
+    user: UserType
+
+
+@strawberry.type
+class LoginError:
+    message: str
+
+
+LoginResult = strawberry.union("LoginResult", (LoginSuccess, LoginError))
+
+
+@strawberry.type
+class UserResponse:
+    username: str
+
+
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def register_or_login(self, info: Info, username: str, email: str, password: str) -> LoginResult:
+        if not User.objects.filter(email=email).exists():
+            user = User.objects.create(username=username, email=email, password=password)
+            user.set_password(password)
+            user.save()
+
+        user = authenticate(email=email, password=password)
+        if not user:
+            return LoginError(message="Something went wrong")
+
+        login(info.context.request, user)
+        return LoginSuccess(user=UserType(username=user.username))
