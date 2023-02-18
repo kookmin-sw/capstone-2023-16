214f3904a3ca4050affe9f606736c65735efaae3
Hoseong Son <lsookcha@me.com>
2023-02-18 16:44:04 +0900

    auth required decorator 추가

--- back/auth_app/schemas/schema.py
+++ back/auth_app/schemas/schema.py
@@ -1,14 +1,34 @@
+from functools import wraps
+from typing import List
+
 import strawberry
 from strawberry import Schema
 from strawberry.tools import merge_types
-
+from strawberry.types import Info
 
 from .user import Mutation as UserMutation
 
 
+def requires_auth(resolver):
+    @wraps(resolver)
+    def auth_wrapper(*args, **kwargs):
+        if not kwargs['info'].context.request.user.is_authenticated:
+            raise AttributeError("auth required")
+        return resolver(args, kwargs)
+
+    return auth_wrapper
+
+
+@requires_auth
+def sample_query_resolver(self, info: Info):
+    return [
+        "A",
+    ]
+
+
 @strawberry.type
 class Query:
-    name: str
+    books: List[str] = strawberry.field(resolver=sample_query_resolver)
 
 
 mutations = (UserMutation,)
