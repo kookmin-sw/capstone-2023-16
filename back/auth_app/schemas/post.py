7dca926a31e85b7aee2e781951cf8db6df7946e8
Hoseong Son <lsookcha@me.com>
2023-02-19 20:37:29 +0900

    글 쓰기 gql.django.connection 으로 구현

--- back/auth_app/schemas/post.py
+++ back/auth_app/schemas/post.py
@@ -1,59 +1,26 @@
-from typing import Optional, Iterable
-
-import strawberry
+from typing import Optional, Iterable, cast
 
 from strawberry.types.info import Info
 from strawberry_django_plus import gql
 from strawberry_django_plus.gql import relay
-
-from auth_app.models import Post as PostModel
-from typing_extensions import Self
-
 from strawberry_django_plus.mutations import resolvers
 
-
-@strawberry.type
-class PostCreateSuccess:
-    post: str
+from auth_app.models import Post as PostModel
 
 
-@strawberry.type
-class PostCreateError:
-    message: str
-
-
-PostCreateResult = strawberry.union("PostCreateResult", (PostCreateSuccess, PostCreateError))
-
-
-@gql.type
-class Post(relay.Node):
-    _id: strawberry.Private[int]
+@gql.django.type(PostModel)
+class PostNode(relay.Node):
     title: str
     content: str
-
-    @classmethod
-    def resolve_id(cls, root: Self, *, info: Optional[Info] = None):
-        return root._id
-
-    @classmethod
-    def resolve_node(cls, node_id: str, info: Optional[Info] = None, required=False):
-        return PostModel.objects.get(pk=node_id)
-
-    @classmethod
-    def resolve_nodes(cls, info: Optional[Info] = None, node_ids: Optional[Iterable[str]] = None):
-        if node_ids is not None:
-            posts = PostModel.objects.filter(id__in=node_ids).values  # FIXME: node_id 를 base64 decode 해야함
-        else:
-            posts = PostModel.objects.all().values()
-
-        # TODO: Author도 fk resolve 되어야 함
-        return map(lambda post: Post(title=post['title'], content=post['content'], _id=post['id']), posts)
 
 
 @gql.type
 class Query:
-    post: Optional[Post] = relay.node()
-    posts_connection: relay.Connection[Post] = relay.connection()
+    post: Optional[PostNode] = relay.node()
+
+    @gql.django.connection
+    def posts_connection(self) -> Iterable[PostNode]:
+        return cast(Iterable[PostNode], PostModel.objects.all())
 
 
 @gql.django.partial(PostModel)
@@ -68,5 +35,5 @@ class CreatePostInput:
 @gql.type
 class Mutation:
     @gql.mutation
-    def post_create(self, info: Info, input: CreatePostInput) -> Post:
+    def post_create(self, info: Info, input: CreatePostInput) -> PostNode:
         return resolvers.create(info, PostModel, resolvers.parse_input(info, vars(input)))
