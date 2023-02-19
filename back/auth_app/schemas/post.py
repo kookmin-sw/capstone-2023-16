6cd2c59ffd7f3cc6e6d837e5ecf2846b981d5fca
Hoseong Son <lsookcha@me.com>
2023-02-19 20:44:49 +0900

    abstract class 상속

--- back/auth_app/schemas/post.py
+++ back/auth_app/schemas/post.py
@@ -1,9 +1,11 @@
-from typing import Optional, Iterable, cast
+from typing import Optional, Iterable, cast, Type
 
 from strawberry.types.info import Info
+from strawberry.utils.await_maybe import AwaitableOrValue
 from strawberry_django_plus import gql
 from strawberry_django_plus.gql import relay
 from strawberry_django_plus.mutations import resolvers
+from strawberry_django_plus.relay import NodeType
 
 from auth_app.models import Post as PostModel
 
@@ -12,6 +14,16 @@ from auth_app.models import Post as PostModel
 class PostNode(relay.Node):
     title: str
     content: str
+
+    @classmethod
+    def resolve_nodes(cls: Type[NodeType], *,
+                      info: Optional[Info] = None, node_ids: Optional[Iterable[str]] = None) \
+            -> AwaitableOrValue[Iterable[NodeType]]:
+        raise NotImplementedError
+
+    @classmethod
+    def resolve_node(cls, node_id: str, *, info: Optional[Info] = None, required: bool = False):
+        raise NotImplementedError
 
 
 @gql.type
@@ -23,7 +35,7 @@ class Query:
         return cast(Iterable[PostNode], PostModel.objects.all())
 
 
-@gql.django.partial(PostModel)
+@gql.django.input(PostModel)
 class CreatePostInput:
     title: gql.auto
     content: gql.auto
