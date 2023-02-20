69bbdcd4fbcdb812eb7619ca9099094a84a37f63
kmujm <pmini09@naver.com>
2023-02-20 23:36:05 +0900

    feat: color palette customize

---
+++ front/mobile/src/components/colors.tsx
@@ -0,0 +1,10 @@
+export const colors = {
+  white: "#fff",
+  primary: "#6461FF",
+  secondary: "#DBBEFF",
+  tertiary: "#FFBBE4",
+  black: "#000000",
+  gray: "#ACACAC",
+  graylight: "#D9D9D9",
+  graydark: "#717171",
+};
