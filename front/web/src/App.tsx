1e8c2ec57fb97135c450b1ea2a9ce7fd86713152
Chaemin-L <chaem12@kookmin.ac.kr>
2023-03-06 21:40:32 +0900

    refactor: 훅을 사용하여 일부 컴포넌트간 종속성 제거

--- front/web/src/App.tsx
+++ front/web/src/App.tsx
@@ -3,20 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import { GlobalStyles } from './styles/GlobalStyles';
 import PostChoice from './pages/PostChoice';
 import LoginPage from './pages/LoginPage';
-import MainLayout from './components/commons/MainLayout';
-import useDeviceType from './hooks/useDeviceType';
 
 const App = () => {
-  const deviceType = useDeviceType();
-
   return (
-    <MainLayout widthType={deviceType}>
+    <>
       <GlobalStyles />
       <Routes>
         <Route path='/' element={<PostChoice/>} />
         <Route path='/login' element={<LoginPage />} />
       </Routes>
-    </MainLayout>
+    </>
   );
 }
 
