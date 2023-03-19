d3f2fff3fdb67671cf7244c7a28f8174bdf4cca3
kmujm <pmini09@naver.com>
2023-03-19 11:29:44 +0900

    feat: .env 추가

--- front/mobile/src/fetchGraphQL.tsx
+++ front/mobile/src/fetchGraphQL.tsx
@@ -1,20 +1,22 @@
+import {REACT_APP_API_URL} from '@env';
+
 /* eslint-disable prettier/prettier */
 // your-app-name/src/fetchGraphQL.js
-async function fetchGraphQL(text:string, variables: object) {
-    // Fetch data from GitHub's GraphQL API:
-    const response = await fetch('https://persona-backend.herokuapp.com/graphql', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        query: text,
-        variables,
-      }),
-    });
-  
-    // Get the response as JSON
-    return await response.json();
-  }
-  
-  export default fetchGraphQL;
+async function fetchGraphQL(text: string, variables: object) {
+  // Fetch data from GitHub's GraphQL API:
+  const response = await fetch(`${REACT_APP_API_URL}/graphql`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: text,
+      variables,
+    }),
+  });
+
+  // Get the response as JSON
+  return await response.json();
+}
+
+export default fetchGraphQL;
