c321ba39b6d59f1b95917b5c4598017727c1e57e
Chaemin-L <chaem12@kookmin.ac.kr>
2023-03-26 14:02:31 +0900

    chore: 일부 속성 이름 변경

--- front/web/src/pages/PostChoice.tsx
+++ front/web/src/pages/PostChoice.tsx
@@ -3,11 +3,11 @@ import PostCard from "../components/commons/PostCard";
 import TextButton from "../components/commons/TextButton";
 
 const PostChoice = () => {
-    const devicetype = useDeviceType();
+    const deviceType = useDeviceType();
 
     // 데이터 받아왔을 때
     const temptitle = '반려동물우린왜고양이를까칠하다고생각할까ㅁ';
-    const tempdate = '2022.11.11 00:00:00';
+    const tempdate = new Date('2022.11.11 00:00:00');
     const tempcontent = '무관심하고까칠하다는이미지는사라지지않는것일까어느정도ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ'
     const temphashtag = ['대학', '조별과제', 'ㅁㅁㅁㅁ', 'ㅁㅁㅁㅁ', 'ㅁㅁ', 'ㅁㅁ'];
 
@@ -26,8 +26,8 @@ const PostChoice = () => {
 
     return(
         <div>
-            <PostCard devicetype={devicetype} title={title} date={tempdate} content={content} hashtag={hashtag}/>
-            <TextButton devicetype={devicetype} text='미리보기'/>
+            <PostCard deviceType={deviceType} title={title} date={tempdate} content={content} hashtag={hashtag}/>
+            <TextButton deviceType={deviceType} text='미리보기'/>
         </div>
     )
 }
