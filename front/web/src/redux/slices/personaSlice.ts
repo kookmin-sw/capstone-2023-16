478a95949448e37ad087f57b609d79776dfa2df1
Chaemin-L <chaem12@kookmin.ac.kr>
2023-03-26 14:13:38 +0900

    fix: 접속된 페르소나 상태 조회 불가 문제 해결

--- front/web/src/redux/slices/personaSlice.ts
+++ front/web/src/redux/slices/personaSlice.ts
@@ -1,26 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 
-export interface PersonaState {
+interface PersonaState {
   id: string
   nickname: string
+  src?: string
 };
 
 const initialState: PersonaState = {
-  id: '',
-  nickname: ''
+    id: '',
+    nickname: '',
+    src: ''
 };
 
 export const personaSlice = createSlice({
   name: 'persona',
   initialState,
   reducers: {
-    connect: (state, action: PayloadAction<PersonaState>) => {
-      state = action.payload;
-    },
-    disconnect: (state) => {
-      state = initialState;
-    },
+    connect: (state, action: PayloadAction<PersonaState>) => ({...state, ...action.payload}),
+    disconnect: (state) => ({...state, ...initialState}),
   },
 });
 
