ca796efc970cb2ae3e81e03dff363d2d42508de6
kmujm <pmini09@naver.com>
2023-04-03 00:26:46 +0900

    feat: redux/toolkit 추가
    slice, store, provider 추가

--- front/mobile/src/App.tsx
+++ front/mobile/src/App.tsx
@@ -1,13 +1,18 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import NavContainer from './navigation/NavContainer';
+
 import {RelayEnvironmentProvider} from 'react-relay';
 import RelayEnvironment from './RelayEnvironment';
 
+import {Provider} from 'react-redux';
+import {store} from './redux/store';
 export default function App() {
   return (
     <RelayEnvironmentProvider environment={RelayEnvironment}>
-      <NavContainer />
+      <Provider store={store}>
+        <NavContainer />
+      </Provider>
     </RelayEnvironmentProvider>
   );
 }
