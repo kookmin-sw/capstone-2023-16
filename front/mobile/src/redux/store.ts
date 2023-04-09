969e2ad0144313b476bab9f646ff6626f559ccf5
kmujm <pmini09@naver.com>
2023-04-10 00:07:37 +0900

    feat: add redux persistStore
    set AsyncStorage as persist storage

--- front/mobile/src/redux/store.ts
+++ front/mobile/src/redux/store.ts
@@ -1,15 +1,32 @@
-import {configureStore, combineReducers} from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit';
+import {PersistConfig, persistStore, persistReducer} from 'redux-persist';
 
 import authReducer from './slices/userSlice';
 
-// const rootReducer = combineReducers({
-//   auth: authReducer,
-// });
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+type ReducerState = ReturnType<typeof rootReducer>;
 
-export const store = configureStore({
-  reducer: {authReducer},
+const persistConfig: PersistConfig<ReducerState> = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['auth'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+  reducer: persistedReducer,
 });
 
+const persistor = persistStore(store);
+
+export {store, persistor};
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type store
