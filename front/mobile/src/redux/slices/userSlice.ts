b34dcf95adaa400c5be9d6852e71b439509c7d62
kmujm <pmini09@naver.com>
2023-04-10 00:09:54 +0900

    fix: auth reducer 수정

--- front/mobile/src/redux/slices/userSlice.ts
+++ front/mobile/src/redux/slices/userSlice.ts
@@ -1,5 +1,5 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {RootState} from '../store';
 
 interface AuthState {
@@ -26,24 +26,35 @@ export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setIsLoggedIn: (state, action: PayloadAction<AuthState['isLoggedIn']>) => {
-      state.isLoggedIn = action.payload;
-    },
-    setUser: (state, action: PayloadAction<AuthState['user']>) => {
+    login: (state, action: PayloadAction<AuthState['user']>) => {
+      state.isLoggedIn = true;
       state.user = action.payload;
+      AsyncStorage.setItem('user', JSON.stringify(action.payload));
     },
     setPersona: (state, action: PayloadAction<AuthState['persona_id']>) => {
       state.persona_id = action.payload;
+      AsyncStorage.setItem('persona_id', JSON.stringify(action.payload));
+    },
+    logout: state => {
+      AsyncStorage.removeItem('user');
+      AsyncStorage.removeItem('persona_id');
+      state.isLoggedIn = false;
+      state.persona_id = '';
+      (state.user = {
+        id: '',
+        username: '',
+        email: '',
+      }),
+        console.log(`@@@@@@@@state : ${JSON.stringify(state)}`);
     },
   },
 });
 
-export const {setUser, setIsLoggedIn, setPersona} = authSlice.actions;
+export const {login, setPersona, logout} = authSlice.actions;
 
-export const selectUser = (state: RootState) => state.authReducer.user;
-export const selectIsLoggedIn = (state: RootState) =>
-  state.authReducer.isLoggedIn;
-export const selectAuth = (state: RootState) => state.authReducer;
-export const selectPersona = (state: RootState) => state.authReducer.persona_id;
+export const selectUser = (state: RootState) => state.auth.user;
+export const selectIsLoggedIn = (state: RootState) => state.auth.isLoggedIn;
+export const selectAuth = (state: RootState) => state.auth;
+export const selectPersona = (state: RootState) => state.auth.persona_id;
 
 export default authSlice.reducer;
