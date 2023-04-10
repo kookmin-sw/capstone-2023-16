726fbb244f6178e9a68a8ddc345e50383fef712a
Hoseong Son <lsookcha@me.com>
2023-04-10 22:16:39 +0900

    Challenge create mutation 추가

--- back/graphql_app/resolvers/challenge/mutations.py
+++ back/graphql_app/resolvers/challenge/mutations.py
@@ -1,15 +1,16 @@
 from strawberry_django_plus.mutations import resolvers
 from strawberry_django_plus import gql
+from strawberry.types import Info
+
+from graphql_app.resolvers.model_types import Challenge
+from graphql_app.resolvers.challenge.types import CreateChallengeObjectiveInput
 
 @gql.type
 class ChallengeMutation:
     @gql.mutation
-    def create_challenge(self, info: Info, input: CreateChallengeObjectiveInput) -> ModelNode:
+    def create_challenge(self, info: Info, input: CreateChallengeObjectiveInput) -> Challenge:
         data = vars(input)
         node_id: gql.relay.GlobalID = data.pop('id')
-        asset: Asset = node_id.resolve_node(info, ensure_type=Asset)
 
-        if asset.owner != info.context.request.user:
-            raise PermissionError("You can only modify objects you own.")
+        return resolvers.create(info, Challenge, resolvers.parse_input(info, data))
 
-        return resolvers.update(info, asset, resolvers.parse_input(info, data))
