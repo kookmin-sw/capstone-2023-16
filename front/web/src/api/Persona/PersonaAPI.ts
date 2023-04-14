53c4442ed2b1350c0a8f2ec4a028659e93aca073
Hoseong Son <lsookcha@me.com>
2023-04-14 17:00:45 +0900

    persona 들고오기 성공

--- front/web/src/api/Persona/PersonaAPI.ts
+++ front/web/src/api/Persona/PersonaAPI.ts
@@ -1,8 +1,8 @@
 
 // GraphQl 
-import { commitMutation, graphql, useFragment, useMutation, usePaginationFragment } from 'react-relay';
+import { commitMutation, useFragment, useLazyLoadQuery, useMutation, usePaginationFragment } from 'react-relay';
+import graphql from 'babel-plugin-relay/macro';
 import environment from '../../RelayEnvironment';
-import PersonasFragment from '../../graphQL/Components/PersonasFragment';
 import PersonaConnection from '../../graphQL/Components/PersonaConnection';
 import {  PersonasFragmentPersonaList_getOwnPersonas$key as Key} from '../../graphQL/Components/__generated__/PersonasFragmentPersonaList_getOwnPersonas.graphql';
 import { PersonasGetQuery } from '../../graphQL/Components/__generated__/PersonasGetQuery.graphql';
@@ -10,12 +10,40 @@ import { PersonasGetQuery } from '../../graphQL/Components/__generated__/Persona
 
 class PersonaAPI {
   public personaListGet = () => {
-    const {data } = usePaginationFragment<PersonasGetQuery, Key>(
-        PersonasFragment, null)
+    const queryData = useLazyLoadQuery(
+      graphql`
+        query PersonaAPIPersonasGetQuery {
+          ...PersonaAPIPersonasFragmentPersonaList_getOwnPersonas
+        }
+      `, []);
+  
+    const { data } = usePaginationFragment<PersonasGetQuery, any>(
+      graphql`
+      fragment PersonaAPIPersonasFragmentPersonaList_getOwnPersonas on Query
+        @refetchable(queryName: "PersonaAPIPersonasGetPaginationQuery")
+        @argumentDefinitions(
+          first: {type: "Int", defaultValue: 10}
+          after: {type: "String"}
+        ) {
+          getOwnPersonas(first: $first, after: $after, sortingOpt: {})
+          @connection(key: "Edges_getOwnPersonas") {
+            edges {
+              node {
+                id
+              }
+            }
+            pageInfo {
+              hasNextPage
+              hasPreviousPage
+              startCursor
+              endCursor
+            }
+          }
+        }
+    `, queryData)
     console.log('data:', data);
     return data;
   };
-  
 }
 
 export default PersonaAPI;
