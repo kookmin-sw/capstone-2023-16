d9c5192d26514a76592f079e65326506b59d26d2
kmujm <pmini09@naver.com>
2023-04-16 01:51:39 +0900

    feat: add welcome screen

--- front/mobile/src/screens/WelcomScreen.tsx
+++ front/mobile/src/screens/WelcomScreen.tsx
@@ -1,15 +1,58 @@
 import React, {FC} from 'react';
-import {ImageBackground} from 'react-native';
 //@ts-ignore
 import styled from 'styled-components/native';
-import {Container} from '../components/common/shared';
-import {imagePath} from '../utils/imagePath';
+import {ImageBackground} from 'react-native';
 
-const WelcomeSection = styled(Container)``;
-export const WelcomeScreen: FC = () => {
+import * as ButtonTheme from '../components/common/theme';
+import RegularButton from '../components/common/Buttons/RegularButton';
+import {DimensionTheme} from '../components/common/shared';
+import {imagePath} from '../utils/imagePath';
+import {colors} from '../components/common/colors';
+import {NavigationData} from '../navigation/AuthNavigator';
+
+const WelcomeSection = styled.View`
+  align-items: center;
+`;
+
+const BottomSection = styled.View`
+  position: absolute;
+  top: 75%;
+`;
+
+type Props = NavigationData<'Welcome'>;
+export const WelcomeScreen: FC<Props> = ({navigation}) => {
   return (
     <WelcomeSection>
-      <ImageBackground source={imagePath.welcome} />
+      <ImageBackground
+        source={imagePath.welcome}
+        style={{width: '100%', height: '100%'}}
+      />
+      <BottomSection>
+        <RegularButton
+          btnStyles={[
+            ButtonTheme.whiteBGpurpleSD.btnStyle,
+            {
+              width: DimensionTheme.width(336),
+              marginBottom: DimensionTheme.width(21),
+            },
+          ]}
+          onPress={() => {
+            navigation.navigate('Login');
+          }}>
+          SIGN IN
+        </RegularButton>
+        <RegularButton
+          btnStyles={[
+            ButtonTheme.purpleBGSD.btnStyle,
+            {width: DimensionTheme.width(336)},
+          ]}
+          textStyles={{color: colors.white}}
+          onPress={() => {
+            navigation.navigate('Signup');
+          }}>
+          SIGN UP
+        </RegularButton>
+      </BottomSection>
     </WelcomeSection>
   );
 };
