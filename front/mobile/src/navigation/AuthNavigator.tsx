21a9e88890c6929459d8e200ca33fa325ad4f454
kmujm <pmini09@naver.com>
2023-04-28 17:02:42 +0900

    remove: delete unused navigator

--- front/mobile/src/navigation/AuthNavigator.tsx
+++ front/mobile/src/navigation/AuthNavigator.tsx
@@ -9,31 +9,17 @@ import {
 } from '@react-navigation/native-stack';
 
 import {LoginScreen} from '../screens/LoginScreen';
-import MainScreen from '../screens/MainScreen';
 import {SignupScreen} from '../screens/SignupScreen';
 import {BaseInfoScreen} from '../screens/Persona/BaseInfoScreen';
 import {InterestTagSettingScreen} from '../screens/Persona/InterestTagSettingScreen';
-import DetailScreen from '../screens/DetailScreen';
-import FilterScreen from '../screens/FilterScreen';
-import ImageButton from '../components/common/Buttons/ImageButton';
-import {MyPageScreen} from '../screens/MyAccount/MyPageScreen';
-import {imagePath} from '../utils/imagePath';
-import {FollowingScreen} from '../screens/MyAccount/FollowingScreen';
-import {MyHistoryScreen} from '../screens/MyHistory/MyHistoryScreen';
-import {SettingScreen} from '../screens/MyAccount/SettingScreen';
+import {WelcomeScreen} from '../screens/WelcomScreen';
 
 export type ParamList = {
+  Welcome: undefined;
   Login: undefined;
   Signup: undefined;
-  Main: undefined;
   BaseInfo: undefined;
   InterestTagSetting: undefined;
-  DetailContent: undefined;
-  FilterContent: undefined;
-  MyPage: undefined;
-  Following: undefined;
-  History: undefined;
-  Setting: undefined;
 };
 
 const StackNavigator = createNativeStackNavigator<ParamList>();
@@ -41,6 +27,11 @@ const StackNavigator = createNativeStackNavigator<ParamList>();
 const AuthNavigator = () => {
   return (
     <StackNavigator.Navigator>
+      <StackNavigator.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{headerShown: false}}
+      />
       <StackNavigator.Screen
         name="Login"
         component={LoginScreen}
@@ -51,11 +42,6 @@ const AuthNavigator = () => {
         component={SignupScreen}
         options={{title: '회원가입', headerShadowVisible: false}}
       />
-      {/* <StackNavigator.Screen
-        name="Main"
-        component={MainScreen}
-        options={{headerShown: false}}
-      /> */}
       <StackNavigator.Screen
         name="BaseInfo"
         component={BaseInfoScreen}
@@ -74,73 +60,6 @@ const AuthNavigator = () => {
           headerBackTitleVisible: false,
         }}
       />
-      {/*<StackNavigator.Screen
-        name="DetailContent"
-        component={DetailScreen}
-        options={{headerShown: false}}
-      />
-      <StackNavigator.Screen
-        name="FilterContent"
-        component={FilterScreen}
-        options={{headerShown: false}}
-      />
-      <StackNavigator.Screen
-        name="MyPage"
-        component={MyPageScreen}
-        options={({navigation}) => ({
-          headerShadowVisible: false,
-          headerTitle: '홍현지',
-          headerTransparent: true,
-          headerTitleStyle: {
-            fontWeight: '700',
-            fontSize: 28,
-          },
-          headerRight: () => (
-            <>
-              <ImageButton
-                btnStyles={{backgroundColor: 'transparent'}}
-                source={imagePath.shareIcon}
-                onPress={() => {}}
-              />
-              <ImageButton
-                btnStyles={{backgroundColor: 'transparent'}}
-                source={imagePath.editIcon}
-                onPress={() => {}}
-              />
-              <ImageButton
-                btnStyles={{backgroundColor: 'transparent'}}
-                source={imagePath.settingIcon}
-                onPress={() => {
-                  navigation.navigate('Setting');
-                }}
-              />
-            </>
-          ),
-        })}
-      />
-      <StackNavigator.Screen
-        name="Following"
-        component={FollowingScreen}
-        options={{
-          title: '팔로잉',
-          headerShadowVisible: false,
-          headerBackTitleVisible: false,
-        }}
-      />
-      <StackNavigator.Screen
-        name="History"
-        component={MyHistoryScreen}
-        options={{
-          title: 'MY HISTORY',
-          headerShadowVisible: false,
-          headerBackTitleVisible: false,
-        }}
-      />
-      <StackNavigator.Screen
-        name="Setting"
-        component={SettingScreen}
-        options={{headerShown: true}}
-      /> */}
     </StackNavigator.Navigator>
   );
 };
