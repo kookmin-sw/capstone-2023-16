8283becea7c5e41f1c3b0986061e610ef6419ef8
kmujm <pmini09@naver.com>
2023-04-28 17:01:53 +0900

    feat: add my content screen

---
+++ front/mobile/src/screens/MyContent/MyContentScreen.tsx
@@ -0,0 +1,51 @@
+import React, {FC, useState} from 'react';
+import {SceneMap} from 'react-native-tab-view';
+//@ts-ignore
+import styled from 'styled-components/native';
+import {Header} from '../../components/common/Header/Header';
+import {Container, DimensionTheme} from '../../components/common/shared';
+import {Tab} from '../../components/common/Tab/Tab';
+import {routeProps, sceneMapProps} from '../../components/common/Tab/type';
+import RegularText from '../../components/common/Texts/RegularText';
+import {NavigationData} from '../../navigation/AppNavigator';
+import {LikeScreen} from '../MyHistory/LikeScreen';
+import {StatisticsScreen} from '../MyHistory/StatisticsScreen';
+
+const ContentContainer = styled(Container)`
+  align-items: flex-start;
+`;
+
+const HeaderSection = styled.View`
+  margin-top: ${DimensionTheme.width(15)};
+  margin-left: ${DimensionTheme.width(11)};
+`;
+
+type Props = NavigationData<'MyContent'>;
+
+export const MyContentScreen: FC<Props> = ({navigation}) => {
+  const [routes] = useState<routeProps[]>([
+    {key: 'all', title: 'ALL'},
+    {key: 'free', title: 'FREE'},
+    {key: 'waiterm', title: 'WAITERM'},
+    {key: 'membership', title: 'MEMBERSHIP'},
+  ]);
+
+  const sceneMaps = SceneMap<sceneMapProps>({
+    all: LikeScreen,
+    free: LikeScreen,
+    waiterm: LikeScreen,
+    membership: StatisticsScreen,
+  });
+
+  return (
+    <ContentContainer>
+      <HeaderSection>
+        <Header navigation={navigation} />
+        <RegularText textStyle={{marginLeft: DimensionTheme.width(20)}}>
+          OOO님의 CONTENT
+        </RegularText>
+      </HeaderSection>
+      <Tab routes={routes} sceneMap={sceneMaps} />
+    </ContentContainer>
+  );
+};
