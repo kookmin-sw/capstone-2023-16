9737194ade80bb9bd182ce97f3c7cc8d4473c69f
kmujm <pmini09@naver.com>
2023-04-28 17:00:52 +0900

    feat: add challenge card

---
+++ front/mobile/src/constants/challengeCard.tsx
@@ -0,0 +1,61 @@
+export const challengeData = [
+  {
+    id: '1',
+    open: true,
+    title: '에세이를 사랑하는 사람들',
+    max: 10,
+    current: 1,
+    body: '요즘 책을 너무 안 읽네요....',
+  },
+  {
+    id: '2',
+    open: false,
+    title: '홍현지 workspace',
+    max: 1,
+    current: 1,
+    body: '나 혼자하는거임.\n하지마셈',
+  },
+  {
+    id: '3',
+    open: false,
+    title: '홍현지 workspace',
+    max: 1,
+    current: 1,
+    body: '나 혼자하는거임.\n하지마셈',
+  },
+  {
+    id: '4',
+    open: false,
+    title: '홍현지 workspace',
+    max: 1,
+    current: 1,
+    body: '나 혼자하는거임.\n하지마셈',
+  },
+];
+
+export const challengeDataRecruit = [
+  {
+    id: '2',
+    open: true,
+    title: '에세이를 사랑하는 사람들',
+    max: 10,
+    current: 1,
+    body: '요즘 책을 너무 안 읽네요....',
+  },
+  {
+    id: '3',
+    open: true,
+    title: '에세이를 사랑하는 사람들',
+    max: 10,
+    current: 1,
+    body: '요즘 책을 너무 안 읽네요....',
+  },
+  {
+    id: '4',
+    open: true,
+    title: '에세이를 사랑하는 사람들',
+    max: 10,
+    current: 1,
+    body: '요즘 책을 너무 안 읽네요....',
+  },
+];
