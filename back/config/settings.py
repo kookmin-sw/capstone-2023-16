4c6436c3b10793c1c3a079d2d3a6f3cf8ddb65a0
pcjs156 <itqkrdbqls1001@naver.com>
2023-05-03 14:41:06 +0900

    feat: 1분마다 log 쌓는 cronjob 생성

--- back/config/settings.py
+++ back/config/settings.py
@@ -50,6 +50,7 @@ DJANGO_DEFAULT_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_crontab',
 ]
 
 CSRF_COOKIE_SECURE = True
@@ -159,3 +160,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CRONTAB
+CRONJOBS = [
+    ('*/1 * * * *', 'config.cron.test', '>> test.log')
+]
