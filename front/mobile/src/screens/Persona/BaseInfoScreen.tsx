3dd0220384111eeae382408eb939909793607012
kmujm <pmini09@naver.com>
2023-05-04 18:04:07 +0900

    fix: 페르소나 생성 페이지 props, formik 추가
    페르소나 생성 -> 태그 생성 페이지로 props 전달

--- front/mobile/src/screens/Persona/BaseInfoScreen.tsx
+++ front/mobile/src/screens/Persona/BaseInfoScreen.tsx
@@ -2,6 +2,9 @@ import React, {FC, useState} from 'react';
 //@ts-ignore
 import styled from 'styled-components/native';
 
+import * as Yup from 'yup';
+import {Formik} from 'formik';
+
 import * as ButtonTheme from '../../components/common/theme';
 import RegularButton from '../../components/common/Buttons/RegularButton';
 import {colors} from '../../components/common/colors';
@@ -58,11 +61,6 @@ const BirthInfoSection = styled.View`
   width: ${DimensionTheme.width(327)};
 `;
 
-const BirthDropdownSection = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
 const GenderInfoSection = styled.View`
   flex-direction: row;
   margin-top: ${DimensionTheme.width(20)};
@@ -81,7 +79,7 @@ const OccupationInfoSection = styled.View`
   flex-direction: row;
   margin-top: ${DimensionTheme.width(15)};
   width: ${DimensionTheme.width(318)};
-  height: ${DimensionTheme.width(43)};
+  height: ${DimensionTheme.width(43)};x
 `;
 
 const PersonaDescriptionSection = styled.View`
@@ -100,70 +98,54 @@ const ButtonSection = styled.View`
   justify-contnet: flex-end;
 `;
 
-let fruits = [
+const occupations = [
   {
-    id: 1,
-    name: 'Mango',
+    id: 'STUDENT',
+    name: '학생',
   },
   {
-    id: 2,
-    name: 'Banana',
+    id: 'EDUCATOR',
+    name: '교육자',
   },
   {
-    id: 3,
-    name: 'Apple',
+    id: 'JOB_SEEKR',
+    name: '취준생',
+  },
+  {
+    id: 'EMPLOYEE',
+    name: '직장인',
+  },
+  {
+    id: 'IT',
+    name: 'IT',
+  },
+  {
+    id: 'FINANCE',
+    name: '금융',
+  },
+  {
+    id: 'ART',
+    name: '예술',
+  },
+  {
+    id: 'ETC',
+    name: '기타',
   },
 ];
 
 type Props = NavigationData<'BaseInfo'>;
 
 export const BaseInfoScreen: FC<Props> = ({navigation}) => {
-  const [isPrivate, setIsPrivate] = useState(false);
+  const [isPublic, setIsPublic] = useState(false);
 
-  // 생년월일
-  const [birth, setBirth] = useState({year: null, month: null, day: null});
-  const now = new Date();
-  var year = now.getFullYear();
-
-  let years = [];
-  //년도 selectbox만들기
-  for (var i = 1970; i <= year; i++) {
-    years.push({id: i.toString(), name: i.toString()});
-  }
-  let months = [];
-  // 월별 selectbox 만들기
-  for (var i = 1; i <= 12; i++) {
-    var mm = i > 9 ? i : '0' + i;
-    months.push({id: mm.toString(), name: mm.toString()});
-  }
-
-  let days = [];
-  // 일별 selectbox 만들기
-  for (var i = 1; i <= 31; i++) {
-    var dd = i > 9 ? i : '0' + i;
-    days.push({id: dd.toString(), name: dd.toString()});
-  }
-
-  const onSelectBirthY = item => {
-    setBirth({
-      ...birth,
-      year: item,
-    });
-  };
-
-  const onSelectBirthM = item => {
-    setBirth({
-      ...birth,
-      month: item,
-    });
-  };
-
-  const onSelectBirthD = item => {
-    setBirth({
-      ...birth,
-      day: item,
-    });
-  };
+  const PersonaSchema = Yup.object().shape({
+    nickname: Yup.string().required('닉네임은 필수입니다.'),
+    birth: Yup.number()
+      .min(1960, '올바른 출생년도를 입력해주세요.')
+      .max(2030, '올바른 출생년도를 입력해주세요.')
+      .typeError('숫자만 입력가능합니다.'),
+    description: Yup.string(),
+  });
 
   // 성별
   const [isMale, setIsMale] = useState(true);
@@ -171,140 +153,168 @@ export const BaseInfoScreen: FC<Props> = ({navigation}) => {
   // 직업
   const [selectedItem, setSelectedItem] = useState(null);
   const onSelect = item => {
+    console.log('11', item);
     setSelectedItem(item);
   };
   return (
     <BaseInfoContainer>
       <KeyboardAvoidingViewContainer>
-        <>
-          <ProfileImageSection>
-            <RegularText textStyle={{textAlign: 'left', marginBottom: 18}}>
-              기본 정보 입력
-            </RegularText>
-            <ProfileImage source={imagePath.avatar} />
-          </ProfileImageSection>
-          <AccountCheckSection>
-            <CheckBox
-              onPress={() => setIsPrivate(!isPrivate)}
-              isChecked={isPrivate}
-              labelStyle={{marginRight: 30, color: colors.graydark}}
-              label={'비공개 계정'}
-            />
-          </AccountCheckSection>
-          <PersonaNickSection>
-            <StyledTextInput
-              label="페르소나 닉네임"
-              labelStyle={{
-                color: colors.black,
-                marginBottom: 7,
-                fontWeight: '700',
-              }}
-              placeholder="닉네임을 입력해주세요."
-            />
-          </PersonaNickSection>
-          <ExtraInfoSection>
-            <BirthInfoSection>
-              <SmallText
-                textStyle={{
-                  color: colors.black,
-                  marginBottom: 7,
-                  fontWeight: '700',
-                }}>
-                생년월일
-              </SmallText>
-              <BirthDropdownSection>
-                <Dropdown
-                  data={years}
-                  onSelect={onSelectBirthY}
-                  value={birth.year}
-                  viewStyles={{width: DimensionTheme.width(131)}}
-                />
-                <Dropdown
-                  data={months}
-                  onSelect={onSelectBirthM}
-                  value={birth.month}
-                  viewStyles={{width: DimensionTheme.width(92)}}
-                />
-                <Dropdown
-                  data={days}
-                  onSelect={onSelectBirthD}
-                  value={birth.day}
-                  viewStyles={{width: DimensionTheme.width(92)}}
-                />
-              </BirthDropdownSection>
-            </BirthInfoSection>
-            <GenderInfoSection>
-              <SmallText
-                textStyle={{
-                  color: colors.black,
-                  marginBottom: 7,
-                  fontWeight: '700',
-                }}>
-                성별
-              </SmallText>
-              <RadioButtonSection>
+        <Formik
+          initialValues={{
+            nickname: '',
+            birth: '',
+            description: '',
+          }}
+          validationSchema={PersonaSchema}
+          onSubmit={({nickname, birth, description}) => {
+            console.log(nickname, birth, description);
+            navigation.navigate('InterestTagSetting', {
+              nickname: nickname,
+              age: birth,
+              gender: isMale ? 'MALE' : 'FEMALE',
+              introduction: description,
+              isPublic: isPublic,
+              job: selectedItem.id,
+            });
+          }}>
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <>
+              <ProfileImageSection>
+                <RegularText textStyle={{textAlign: 'left', marginBottom: 18}}>
+                  기본 정보 입력
+                </RegularText>
+                <ProfileImage source={imagePath.avatar} />
+              </ProfileImageSection>
+              <AccountCheckSection>
                 <CheckBox
-                  onPress={() => setIsMale(true)}
-                  isChecked={isMale}
-                  labelStyle={{marginRight: 30}}
-                  label={'남'}
+                  onPress={() => setIsPublic(!isPublic)}
+                  isChecked={isPublic}
+                  labelStyle={{marginRight: 30, color: colors.graydark}}
+                  label={'비공개 계정'}
                 />
-                <CheckBox
-                  onPress={() => setIsMale(false)}
-                  isChecked={isMale ? false : true}
-                  label={'여'}
+              </AccountCheckSection>
+              <PersonaNickSection>
+                <StyledTextInput
+                  label="페르소나 닉네임"
+                  labelStyle={{
+                    color: colors.black,
+                    marginBottom: 7,
+                    fontWeight: '700',
+                  }}
+                  placeholder="닉네임을 입력해주세요."
+                  value={values.nickname}
+                  onChangeText={handleChange('nickname')}
+                  onBlur={handleBlur('nickname')}
+                  error={errors.nickname}
+                  touched={touched.nickname}
                 />
-              </RadioButtonSection>
-            </GenderInfoSection>
-            <OccupationInfoSection>
-              <SmallText
-                textStyle={{
-                  color: colors.black,
-                  fontWeight: '700',
-                  marginTop: DimensionTheme.width(15),
-                  marginRight: DimensionTheme.width(25),
-                }}>
-                직업
-              </SmallText>
-              {/* <StyledTextInput
+              </PersonaNickSection>
+              <ExtraInfoSection>
+                <BirthInfoSection>
+                  <StyledTextInput
+                    keyboardType="numeric"
+                    label="출생년도"
+                    labelStyle={{
+                      color: colors.black,
+                      marginBottom: 7,
+                      fontWeight: '700',
+                    }}
+                    labelExtra="나와 비슷한 나이대 관심사를 추천 받을 수 있어요!"
+                    placeholder="출생년도를 입력해주세요. 예)2000"
+                    value={values.birth}
+                    onChangeText={handleChange('birth')}
+                    onBlur={handleBlur('birth')}
+                    error={errors.birth}
+                    touched={touched.birth}
+                  />
+                </BirthInfoSection>
+                <GenderInfoSection>
+                  <SmallText
+                    textStyle={{
+                      color: colors.black,
+                      marginBottom: 7,
+                      fontWeight: '700',
+                    }}>
+                    성별
+                  </SmallText>
+                  <RadioButtonSection>
+                    <CheckBox
+                      onPress={() => setIsMale(true)}
+                      isChecked={isMale}
+                      labelStyle={{marginRight: 30}}
+                      label={'남'}
+                    />
+                    <CheckBox
+                      onPress={() => setIsMale(false)}
+                      isChecked={isMale ? false : true}
+                      label={'여'}
+                    />
+                  </RadioButtonSection>
+                </GenderInfoSection>
+                <OccupationInfoSection>
+                  <SmallText
+                    textStyle={{
+                      color: colors.black,
+                      fontWeight: '700',
+                      marginTop: DimensionTheme.width(15),
+                      marginRight: DimensionTheme.width(25),
+                    }}>
+                    직업
+                  </SmallText>
+                  {/* <StyledTextInput
                 viewStyle={{
                   width: DimensionTheme.width(281),
                   marginLeft: DimensionTheme.width(12),
                 }}
               /> */}
-              <Dropdown
-                data={fruits}
-                onSelect={onSelect}
-                value={selectedItem}
-                viewStyles={{width: DimensionTheme.width(278)}}
-              />
-            </OccupationInfoSection>
-          </ExtraInfoSection>
-          <PersonaDescriptionSection>
-            <StyledTextInput
-              multiline={true}
-              textAlignVertical="top"
-              label="페르소나 소개"
-              labelStyle={{
-                color: colors.black,
-                marginBottom: 7,
-                fontWeight: '700',
-              }}
-              viewStyle={{height: 120}}
-              placeholder="소개말을 입력해주세요."
-            />
-          </PersonaDescriptionSection>
-          <ButtonSection>
-            <RegularButton
-              btnStyles={[ButtonTheme.purpleBG.btnStyle]}
-              textStyles={ButtonTheme.purpleBG.textStyle}
-              onPress={() => {
-                navigation.navigate('InterestTagSetting');
-              }}>
-              확인
-            </RegularButton>
-          </ButtonSection>
-        </>
+                  <Dropdown
+                    data={occupations}
+                    onSelect={onSelect}
+                    value={selectedItem}
+                    viewStyles={{width: DimensionTheme.width(278)}}
+                  />
+                </OccupationInfoSection>
+              </ExtraInfoSection>
+              <PersonaDescriptionSection>
+                <StyledTextInput
+                  multiline={true}
+                  textAlignVertical="top"
+                  label="페르소나 소개"
+                  labelStyle={{
+                    color: colors.black,
+                    marginBottom: 7,
+                    fontWeight: '700',
+                  }}
+                  viewStyle={{height: 120, zIndex: -1}}
+                  placeholder="소개말을 입력해주세요."
+                  value={values.description}
+                  onChangeText={handleChange('description')}
+                  onBlur={handleBlur('description')}
+                  error={errors.description}
+                  touched={touched.description}
+                />
+              </PersonaDescriptionSection>
+              <ButtonSection>
+                <RegularButton
+                  btnStyles={[ButtonTheme.purpleBG.btnStyle]}
+                  textStyles={ButtonTheme.purpleBG.textStyle}
+                  onPress={() => {
+                    handleSubmit();
+                  }}>
+                  확인
+                </RegularButton>
+              </ButtonSection>
+            </>
+          )}
+        </Formik>
       </KeyboardAvoidingViewContainer>
     </BaseInfoContainer>
   );
