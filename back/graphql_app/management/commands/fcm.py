ec9009e8a66d8343765edc42b2d6c587dc24e09c
pcjs156 <itqkrdbqls1001@naver.com>
2023-05-09 18:03:01 +0900

    .

--- back/graphql_app/management/commands/fcm.py
+++ back/graphql_app/management/commands/fcm.py
@@ -1,15 +1,11 @@
-from django.core.management.base import BaseCommand, CommandError
-
-from graphql_app.management.commands._private import get_targets
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'THIS IS PASSWORD'
+    help = 'hihi'
 
     def add_arguments(self, parser):
         pass
 
     def handle(self, *args, **options):
-        targets = get_targets()
-        for target in targets:
-            self.stdout.write(str(target))
+        self.stdout.write("HELLO!")
