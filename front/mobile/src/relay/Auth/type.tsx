1e6814f48c844415582979f7cc228fe783642e94
kmujm <pmini09@naver.com>
2023-05-12 01:24:07 +0900

    :truck: remove unused repository

--- front/mobile/src/relay/Auth/type.tsx
+++
@@ -1,6 +0,0 @@
-export enum Error {
-  UsernameAlreadyUsedError = 'UsernameAlreadyUsedError',
-  WrongCertInfoError = 'WrongCertInfoError',
-  AdminPermissionRequiredError = 'AdminPermissionRequiredError',
-  EmailAlreadyUsedError = 'EmailAlreadyUsedError',
-}
