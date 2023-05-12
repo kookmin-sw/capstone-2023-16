78296e06f46674f2cf4b404533484d74b5a941e2
Hoseong Son <lsookcha@me.com>
2023-05-12 15:47:03 +0900

    코멘트 카운트 추가 (#85)

--- back/graphql_app/domain/post/core.py
+++ back/graphql_app/domain/post/core.py
@@ -198,6 +198,12 @@ def get_comments_of(root: Post, info: Info) -> List[Comment]:
     """
     return Comment.objects.filter(post=root)
 
+def get_comments_count(root: Post, info: Info) -> int:
+    """
+    특정 게시물의 댓글 목록을 작성 일시 내림차순으로 반환
+    """
+    return Comment.objects.filter(post=root).count
+
 
 def create_comment_to(post_id: int, persona_id: int, body) -> Comment:
     return Comment.objects.create(post_id=post_id, persona_id=persona_id, body=body)
