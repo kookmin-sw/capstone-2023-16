7ab3b20c34c23d199a7b20a32f2a922d359fb599
Chaemin-L <chaem12@kookmin.ac.kr>
2023-05-12 16:36:55 +0900

    style: Tag 컴포넌트 구현

---
+++ front/web/src/components/PersonaCreation/TagCard.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import useDeviceType from '../../hooks/useDeviceType';
+
+type TagCard = {
+  id: string,
+  body: string,
+}
+
+const TagCard = ({id, body}: TagCard) => {
+  const deviceType = useDeviceType();
+
+  return <div>
+    <HiddenInput type='checkbox' id={id} />
+    <Label htmlFor={id} className='field__container' deviceType={deviceType}>{body}</Label>
+  </div>
+};
+
+export default TagCard;
+
+const HiddenInput = styled.input`
+  display: none;
+  &:checked {
+    & + label {
+      background-color: #F1DBFF;
+    }
+  }
+`;
+
+const Label = styled.label<{ deviceType: string }>`
+  width: auto;
+  height: ${props => props.deviceType === 'mobile' ? '26px' : '42px'};
+  padding: ${props => props.deviceType === 'mobile' ? '8px' : '15px'};
+  font-size: ${props => props.deviceType === 'mobile' ? '12px' : '20px'};
+  &:hover {
+    cursor: pointer;
+  }
+  &:active {
+    cursor: default;
+  }
+`;
