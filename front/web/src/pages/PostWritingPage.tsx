fbbf4dfe1c67f45f3778bfb7cf94a8889d647586
Chaemin-L <chaem12@kookmin.ac.kr>
2023-05-23 04:07:59 +0900

    refactor: 불필요한 함수 재생성 방지

--- front/web/src/pages/PostWritingPage.tsx
+++ front/web/src/pages/PostWritingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 import styled  from 'styled-components';
@@ -46,6 +46,8 @@ const PostWritingPage = () => {
     setSubmitFlag(false);
   }, [submitFlag]);
 
+  const onSubmit = useCallback(() => setSubmitFlag(prev => true), []);
+
   return <>
     <PersonaCardWrapper deviceType={deviceType} onClick={() => navigate('/personas')}>
       <PersonaCard {...{nickname:persona.nickname, ...location.state }} deviceType={deviceType} />
@@ -53,7 +55,7 @@ const PostWritingPage = () => {
     <ContentLayout>
       <Header deviceType={deviceType}>
         <CategorySelector />
-        <PostWritingSetting setSubmitFlag={setSubmitFlag} />
+        <PostWritingSetting onSubmit={onSubmit} />
       </Header>
       <PostTitle submitFlag={submitFlag} />
       <TextEditor submitFlag={submitFlag} />
